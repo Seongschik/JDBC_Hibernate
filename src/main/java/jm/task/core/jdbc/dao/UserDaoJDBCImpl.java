4e8393b0ad74dd9672196890711ead13c7ef8839
shiga <hikikomorim@gmail.com>
2023-05-29 15:39:43 +0900

    Работает только 4 теста из 6. Голову сломал - не работал

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,78 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private Connection connection;
+    public UserDaoJDBCImpl() throws SQLException{
+        connection = Util.getConnection();
+    }
+    @Override
+    public void createUsersTable() throws SQLException{
+        String query = "CREATE TABLE IF NOT EXISTS users ("
+                + "id INT PRIMARY KEY AUTO_INCREMENT,"
+                + "name VARCHAR(50) NOT NULL,"
+                + "lastName VARCHAR(50) NOT NULL,"
+                + "age TINYINT NOT NULL"
+                + ")";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        }
+    }
+
+    public void dropUsersTable() throws SQLException{
+        String query = "DROP TABLE IF EXISTS users";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        String query = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+        }
+    }
+
+    public void removeUserById(long id) throws SQLException{
+            String query = "DELETE FROM users WHERE id = ?";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setLong(1, id);
+                statement.executeUpdate();
+        }
 
     }
 
-    public void createUsersTable() {
-
+    public List<User> getAllUsers() throws SQLException{
+        List<User> userList = new ArrayList<>();
+        String query = "SELECT * FROM users";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                userList.add(new User(id, name, lastName, age));
+            }
+        }
+        return userList;
     }
 
-    public void dropUsersTable() {
-
+    public void cleanUsersTable() throws SQLException{
+        String query = "TRUNCATE TABLE users";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        }
     }
-
-    public void saveUser(String name, String lastName, byte age) {
-
-    }
-
-    public void removeUserById(long id) {
-
-    }
-
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    public void cleanUsersTable() {
-
+    public void close() throws SQLException {
+        connection.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,50 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+    private UserDaoJDBCImpl userDaoJDBCImpl;
 
+    {
+        try {
+            userDaoJDBCImpl = new UserDaoJDBCImpl();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void dropUsersTable() {
 
+    @Override
+    public void createUsersTable() throws SQLException {
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    @Override
+    public void dropUsersTable() throws SQLException {
     }
 
-    public void removeUserById(long id) {
-
+    @Override
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        User user = new User(name, lastName, age);
+        userDaoJDBCImpl.saveUser(name, lastName, age);
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    @Override
+    public void removeUserById(long id) throws SQLException {
+        userDaoJDBCImpl.removeUserById(id);
     }
 
-    public void cleanUsersTable() {
+    @Override
+    public List<User> getAllUsers() throws SQLException {
+        return userDaoJDBCImpl.getAllUsers();
+    }
 
+    @Override
+    public void cleanUsersTable() throws SQLException {
+        userDaoJDBCImpl.cleanUsersTable();
     }
 }
